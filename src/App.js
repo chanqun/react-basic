2247871073a7537136c1d0248bd94da2f3797fde
chanqun <cksgns93@naver.com>
2022-01-12 19:19:00 +0900

    props와 state 실습 중

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import TOC from "./components/TOC"
 import Content from "./components/Content"
 import Subject from "./components/Subject"
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Subject title="WEB" sub="World wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Languages."></Content>
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            subject: {title:"WEB", sub:"World Wide Web!"},
+            contents:[
+                {id:1, title:'HTML', desc:"HTML is for Information"},
+                {id:2, title:'CSS', desc:"CSS is for Design"},
+                {id:3, title:'JavaScript', desc:"JavaScript is for interactive"}
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}>
+                </Subject>
+                <Subject title="React" sub="For UI"></Subject>
+                <TOC></TOC>
+                <Content title="HTML" desc="HTML is HyperText Markup Languages."></Content>
+            </div>
+        )
+    }
 }
 
 export default App;
