00b0c395520926082f4bd55592e68c773621c37f
chanqun <cksgns93@naver.com>
2022-01-30 15:49:18 +0900

    이벤트 학습 중

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ class App extends Component {
         super(props);
         this.state = {
             mode: 'welcome',
-            subject: {title: "WEB", sub: "World Wide Web!"},
+            subject: {title: 'WEB', sub: 'World Wide Web!'},
             welcome: {title: 'Welcome', desc: 'Hello, React!!!'},
             contents: [
-                {id: 1, title: 'HTML', desc: "HTML is for Information"},
+                {id: 1, title: 'HTML', desc: 'HTML is for Information'},
                 {id: 2, title: 'CSS', desc: "CSS is for Design"},
-                {id: 3, title: 'JavaScript', desc: "JavaScript is for interactive"}
+                {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
             ]
         }
     }
 
     render() {
-        var _title, _desc = null;
+        let _title, _desc = null;
         if (this.state.mode === 'welcome') {
             _title = this.state.welcome.title;
             _desc = this.state.welcome.desc;
@@ -31,12 +31,16 @@ class App extends Component {
         return (
             // props 나 state 가 바뀌면 화면이 달라진다.
             <div className="App">
-                <Subject
-                    title={this.state.subject.title}
-                    sub={this.state.subject.sub}>
-                </Subject>
+                {/*<Subject*/}
+                {/*    title={this.state.subject.title}*/}
+                {/*    sub={this.state.subject.sub}>*/}
+                {/*</Subject>*/}
+                <header>
+                    <h1><a href="/">{this.state.subject.title}</a></h1>
+                    {this.state.subject.sub}
+                </header>
                 <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText Markup Languages."></Content>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         )
     }
